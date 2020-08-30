77c0ca987743032d6b69eecf52cffee6ba943667
krisshen <kris1126@163.com>
2020-08-30 20:59:54 +1000

    launch template and auto scaling group, now in a good shape to provision asg with elb and 2 instances behind it.

--- prod.tf
+++ prod.tf
@@ -3,12 +3,29 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-20200830"
-  acl = "private"
-}
+//resource "aws_s3_bucket" "prod_tf_course" {
+//  bucket = "tf-course-20200830"
+//  acl = "private"
+//  tags = {
+//    "Terraform": "true"
+//  }
+//}
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "ap-southeast-2a"
+  tags = {
+    "Terraform": "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "ap-southeast-2b"
+  tags = {
+    "Terraform": "true"
+  }
+}
 
 resource "aws_security_group" "prod_web" {
   name = "prod_web"
@@ -39,29 +56,77 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami = "ami-038630edbff4d358e"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-
-  tags = {
-    "Terraform": "true"
-  }
-}
+//resource "aws_instance" "prod_web" {
+//  count = 2
+//
+//  ami = "ami-038630edbff4d358e"
+//  instance_type = "t2.nano"
+//
+//  vpc_security_group_ids = [
+//    aws_security_group.prod_web.id
+//  ]
+//
+//  tags = {
+//    "Terraform": "true"
+//  }
+//}
 
 // use aws_eip_association do decouple eip and instance creation
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
+//resource "aws_eip_association" "prod_web" {
+//  instance_id = aws_instance.prod_web.0.id
+//  allocation_id = aws_eip.prod_web.id
+//}
+//
+//resource "aws_eip" "prod_web" {
+//  tags = {
+//    "Terraform": "true"
+//  }
+//}
 
-resource "aws_eip" "prod_web" {
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
   tags = {
     "Terraform": "true"
   }
+}
+
+resource "aws_launch_template" "prod_web" {
+  name_prefix = "prod-web"
+  image_id = "ami-038630edbff4d358e"
+  instance_type = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.prod_web.id]
+  tags = {
+    "Terraform": "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+//  availability_zones = ["ap-southeast-2a", "ap-southeast-2b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  max_size = 2
+  min_size = 2
+
+  launch_template {
+    id = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key = "Terraform"
+    value = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb = aws_elb.prod_web.id
 }
